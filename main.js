e1d14ad8088fc2c10a11e7da64de347e3bff1afe
hugogaytan10 <estardus9@gmail.com>
2023-05-14 20:54:17 -0600

    label on main cards

--- main.js
+++ main.js
@@ -1,39 +1,51 @@
 let productGalery = [
     {
-        photo: './assets/cart.png',
-        product: "CART",
+        photo: './assets/coffe.jpg',
+        product: "COFFE",
         description: "Mini descripcion",
-        amount: 11
+        amount: 11,
+        category: 'categoria1',
+        price: 200
     },
     {
         photo: './assets/tofu.png',
         product: "TOFU",
         description: "Mini descripcion",
-        amount: 5
+        amount: 5,
+        category: 'categoria1',
+        price: 100
     },
     {
-        photo: './assets/portada.png',
-        product: "PORTADA",
+        photo: './assets/queso.jpg',
+        product: "QUESO",
         description: "Mini descripcion",
-        amount: 12
+        amount: 12,
+        category: 'categoria2',
+        price: 200
     },
     {
         photo: './assets/tofu.png',
         product: "TOFU",
         description: "Mini descripcion",
-        amount: 5
+        amount: 5,
+        category: 'categoria2',
+        price: 400
     },
     {
-        photo: './assets/portada.png',
+        photo: './assets/chai.jpg',
         product: "PORTADA",
         description: "Mini descripcion",
-        amount: 12
+        amount: 12,
+        category: 'categoria1',
+        price: 200
     },
     {
-        photo: './assets/cart.png',
+        photo: './assets/queso.jpg',
         product: "CART",
         description: "Mini descripcion",
-        amount: 11
+        amount: 11,
+        category: 'categoria2',
+        price: 40
     },
 ];
 
@@ -69,7 +81,7 @@ function openMenu() {
 function slider() {
     let bgClassHighProduct = 'relative block bg-azulBasico h-80 card  mt-4 rounded-3xl shadow-lg transition-all';
     let bgClassLowProduct = 'relative bg-red-400 h-80 card  mt-4 rounded-3xl shadow-lg transition-all';
-    let imgClass = 'rounded-full h-1/2 absolute left-1/2 -translate-x-1/2 top-2 transition-all hover:scale-105';
+    let imgClass = 'rounded-full z-0 h-1/2 absolute left-1/2 -translate-x-1/2 top-2 transition-all hover:scale-105';
     let divInfoClass = 'bg-white border-gray-200 border-2 absolute w-full bottom-0 rounded-3xl flex flex-wrap justify-center p-2 gap-2';
     let descriptionClass = 'text-grisBasico texto-portada block w-full';
     let buttomHighProduct = 'bg-azulBasico text-gray-100 texto-portada w-8/12 h-6  rounded-sm md:h-8  hover:opacity-75';
@@ -82,6 +94,16 @@ function slider() {
         const item = document.createElement('div');
         item.id = 'cardProduct';
         item.className = product.amount > 10 ? bgClassHighProduct : bgClassLowProduct;
+
+        const label = document.createElement('img');
+        label.src = './assets/label.png';
+        label.className = 'h-32 w-32 absolute z-10 -top-4 right-0 label md:-right-10';
+        item.appendChild(label);
+
+        const price = document.createElement('p');
+        price.innerHTML = product.price;
+        price.className = 'text-white z-10 text-3xl shadow-lg absolute right-8 top-10 md:-right-4'
+        item.appendChild(price);
 
         //se crea la etiqueta para la imagen y sus clases
         const img = document.createElement('img');
